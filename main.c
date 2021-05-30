68d07335fa1f94b0cfc478204c22e34625a110b6
Y7000P-chen <chenjunyu_cqupt@outlook.com>
2021-05-30 19:19:47 +0800

    疫苗表显示全部完成，测试通过

--- main.c
+++ main.c
@@ -42,6 +42,7 @@ typedef struct thing{
 void menu1();
 void add_vaccine();
 int check_vaccine_num();
+void display_vaccine();
 
 int main(){
     int flag1=1,flag2=1,flag3=1;/* flag是判断条件,flag为 1 时为真，为 0 时为假 */
@@ -67,7 +68,7 @@ int main(){
 		fclose(p3);
 	}
 
-    add_vaccine();
+    display_vaccine();
 
 }
 
@@ -185,4 +186,48 @@ int check_vaccine_num(){
     }
     fclose(p1);
     return vaccine_num;
+}
+
+void display_vaccine()/*显示全部疫苗信息*/
+{
+    FILE *p1;
+    int n;
+    VACCINE newvaccine[1000];
+    int vaccine_num=0;
+    system("cls");
+    fflush(stdin);
+    p1=fopen("vaccine.txt","r");
+    while(!feof(p1))
+    {
+      	fscanf(p1,"%d %s %s %d %d %d %d\n",
+                        &newvaccine[vaccine_num].vaccine_id, 
+                        newvaccine[vaccine_num].vaccine_name, newvaccine[vaccine_num].company,
+                        &newvaccine[vaccine_num].allowed_age[0], &newvaccine[vaccine_num].allowed_age[1],
+                        &newvaccine[vaccine_num].times, &newvaccine[vaccine_num].is_used);  
+      	vaccine_num++;
+    }
+    fclose(p1);
+    printf("当前系统内有%d条疫苗记录\n\n",vaccine_num);
+    if(vaccine_num==0)
+    {
+      	printf("\n没有任何疫苗信息!\n\n");
+    }
+    else
+    {
+      	n=0;
+      	printf("|---------------------------------疫苗信息----------------------------------|\n");
+      	printf("|编号  疫苗名      公司      允许接种年龄     需要接种的次数   是否使用了   |\n");
+      	printf("|                                                                           |\n");
+      	while(n<vaccine_num)
+        {
+        	printf("|%-6d%-12s%-10s%-8d~%-16d%-14d%-8d|\n",
+                    newvaccine[n].vaccine_id, 
+                    newvaccine[n].vaccine_name, newvaccine[n].company,
+                    newvaccine[n].allowed_age[0], newvaccine[n].allowed_age[1],
+                    newvaccine[n].times, newvaccine[n].is_used);
+        	n++;
+        }
+      }
+    printf("\n\n按任意键返回上一级菜单!\n");
+   	getch();
 }
